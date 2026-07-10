f84ef6c05d63e4b723564288b1198cc63b817461
Bharat Gupta <bharat@bornwest.com>
2026-07-10 14:06:57 +0530

    Init gem

---
+++ lib/viewkit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "viewkit/version"
+
+module Viewkit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/viewkit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Viewkit
+  VERSION = "0.1.0"
+end
